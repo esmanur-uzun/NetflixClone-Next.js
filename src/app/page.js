1dbc5411c99e13742a8f5be3a5412362624654ba
esmanur-uzun <esmabitmez@gmail.com>
2024-02-29 17:55:56 +0300

    Worked on movies page

--- src/app/page.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import HomeContainer from '@/containers/home'
-
-export default function HomePage() {
-  return (
-   <HomeContainer/>
-  )
-}
--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -3,12 +3,15 @@ import Movies from "@/mocks/movies.json"
 import Genres from '@/mocks/genres.json'
 import { FeaturedMovie } from '@/components/featured-movie/featured'
 import {Categories} from '@/components/categories/categories'
+import { MoviesSection } from '@/components/movies-section'
 
-export default function HomeContainer() {
+export default function HomeContainer(selectedCategory) {
   return (
     <div>
       <FeaturedMovie movie={Movies.results[0]}/>
       <Categories categories={Genres.genres.slice(0,5)}/>
+      <MoviesSection title="Popular Films" movies={Movies.results.slice(1,7)}/>
+      <MoviesSection title="Your Favorites" movies={Movies.results.slice(7,13)}/>
     </div>
   )
 }
